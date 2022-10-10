26d4034d2b42a9a2afc4748cdc361cd86b89c6c1
quocbahuynh <jbrteamwebsite@gmail.com>
2022-10-10 20:49:11 +0700

    rounded searchbar

--- components/form/SearchBar.tsx
+++ components/form/SearchBar.tsx
@@ -15,7 +15,7 @@ export const SearcBar: FC<SearchProps> = ({ handleSearch, searchValue, onChangeS
             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
           </div>
 
-          <input type="search" id="default-search" className="block p-4 pl-10 w-full text-md font-semibold font-sans text-blue-500 bg-gray-50 rounded-lg border-0 border-gray-300 focus:ring-blue-500 focus:border-blue-400  dark:placeholder-gray-200" placeholder="Container Number" defaultValue={searchValue} required onChange={(e) => onChangeSearch(e.target.value)} />
+          <input type="search" id="default-search" className="block p-4 pl-10 w-full text-md font-semibold font-sans text-blue-500 bg-gray-50 rounded-sm border-0 border-gray-300 focus:ring-blue-500 focus:border-blue-400  dark:placeholder-gray-200" placeholder="Container Number" defaultValue={searchValue} required onChange={(e) => onChangeSearch(e.target.value)} />
 
           <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleSearch}>Search</button>
         </div>
