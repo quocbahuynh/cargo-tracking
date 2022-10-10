9f28dffd52969bfd5262b1e880a5661a1166daf3
quocbahuynh <jbrteamwebsite@gmail.com>
2022-10-10 18:40:33 +0700

    remove icon

--- components/data-table/Location.tsx
+++ components/data-table/Location.tsx
@@ -3,38 +3,6 @@ import { convertISO } from '../../utils/time'
 interface Icon {
     status: string,
 }
-const RkemIcon: FC<Icon> = ({ status }) => {
-    switch (status) {
-        case "GATE-OUT":
-            return (
-                <>
-                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" /></svg>
-                </>
-            )
-        case "GATE-IN":
-            return (
-                <>
-                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" /></svg>
-                </>
-            )
-
-        case "LOAD" || "DISCHARG":
-            return (
-                <>
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
-                </>
-            )
-        case "DISCHARG":
-            return (
-                <>
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" /></svg>
-                </>
-            )
-
-        default:
-            break;
-    }
-}
 
 export const Location: FC<any> = ({ event, terminal, lastest }) => {
 
